7505e1a59f7b20e322db5e534d2fb5e40cfded8c
Hasan5352 <hasanlokhandwala27@gmail.com>
2025-01-13 14:32:01 +0100

    updated project

--- index.js
+++ index.js
@@ -2,13 +2,65 @@
 const express = require("express") ;
 const app = express();
 const path = require("path");
+const { v4: uuidv4 } = require('uuid');
+
+app.set("view engine", "ejs")
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const port = 3000;
+const port = 8080;
 
 app.listen(port, ()=>{
     console.log(`App listening on port = ${port}`);
+})
+
+//----------------------------- GET ------------------------------
+
+app.get("/", (req, res)=>{
+    res.send(`<h1> Received GET request </h1>`);
+})
+
+let posts = [
+    {
+        id: uuidv4(),
+        username: "adam",
+        content: `My name is adam` 
+    },
+]
+app.get("/posts", (req, res)=>{                     // index route
+    res.render("index.ejs", {posts});
+})
+
+app.get("/posts/new", (req, res)=>{                     // create route
+    res.render("new.ejs");
+})
+
+app.get("/posts/:id", (req, res)=>{                     // show details route
+    let id = req.params.id;
+    let post = posts.find((p) => id === p.id);
+    res.render("show.ejs", {post});
+})
+
+
+//----------------------------- POST ------------------------------
+
+app.post("/posts", (req, res)=>{                     // create post
+    let {username, content} = req.body;
+    posts.unshift({username, content, id:uuidv4()});
+    res.redirect("/posts");
+})
+
+//----------------------------- patch ------------------------------
+
+app.patch("/posts/:id", (req, res)=>{                     // create post
+    let {username, content} = req.body;
+    posts.unshift({username, content, id:uuidv4()});
+    res.redirect("/posts");
+})
+
+//----------------------------- NONEXISTENT PATH ------------------------------
+app.use("*", (req, res)=>{
+    res.send(`<h1> Page does not exist </h1>`);
 })
